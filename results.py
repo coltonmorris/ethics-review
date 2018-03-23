a277382020d421006a6ef97b949e6d473f2a52e5
Brady Killeen <brady.killeen@tcnbroadcasting.com>
2018-03-23 13:17:20 -0600

    changrs

--- results.py
+++ results.py
@@ -14,8 +14,9 @@ output = output.split("\n\n")
 output[0] = output[0].replace("\n", " ")
 
 for i in range(1,len(output)):
+    print output[0]+" "+output[i]
     r = requests.get('http://www.google.com/search',
-            params={'q':'"'+output[0]+output[i]+'"',
+            params={'q':'"'+output[0]+" "+output[i]+'"',
                 "tbs":"li:1"}
             )
 
