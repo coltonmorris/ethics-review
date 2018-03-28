8f9282ff1e5f3b7f4bb7bd456e91fcc23732baa7
Brady Killeen <brady.killeen@tcnbroadcasting.com>
2018-03-28 16:58:13 -0600

    wikipedia

--- methods/Wikipedia.js
+++ methods/Wikipedia.js
@@ -2,20 +2,42 @@ import { Promise } from 'bluebird'
 import _ from 'lodash'
 import { Observable } from 'rxjs/Rx'
 import { mergeMap, map } from 'rxjs/operators'
-import puppeteer from 'puppeteer'
-
+import axios from 'axios'
+let WordPOS = require('wordpos')
 
 export default (question, answers) => {
-  return Observable.forkJoin(
-    ..._.map(answers, (answer) => (
-      Observable.from(getResults(question, answer))
-    ))).map(results => {
-      return transfromResults(results)
+  return Observable.from(getNouns(question))
+    .map(nouns => {
+      console.log('~~nouns', nouns) //ok
+      return Observable.forkJoin(_.map(answers, (answer) => {
+          console.log('iter', answer)
+          return Observable.from(getBody(nouns, answer))
+          .map((val) => {
+            console.log('^^^^^^^', val)
+            return val
+          })
+      })).map(results => {
+          return transfromResults(results)
+        })
     })
 }
 
-let getResults =  async (question, answer) => {
-  return { answer: answer, count: 0 }
+let getNouns = async(question) => {
+  let wordpos = new WordPOS()
+  return await wordpos.getNouns(question, nouns => nouns)
+}
+
+const count = (str, ans) => {
+  let re = new RegExp(ans, "gi")
+  return str.match(re).length
+}
+
+let getBody = async(questionNouns, answer) => {
+  let transAnswer = _.replace(_.escape(answer), " ", "%20")
+  let url = `https://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&titles=${transAnswer}`
+
+  console.log('before awiat')
+  return await axios.get(url)
 };
 
 let transfromResults = (results) => {
@@ -29,18 +51,12 @@ let transfromResults = (results) => {
     return 0;
   })
 
-  // check if count is 0
-  results = _.map(results, (result) => {
-    if (result.count == 0) return { ...result, count: 1 }
-    return result
-  })
-
   let sum = _.reduce(results, (val, ele) => (val + parseInt(ele.count)), 0)
 
   let final = {
-    smallest: { answer: results[0].answer, weight: results[0].count/sum },
-    middle: { answer: results[1].answer, weight: results[1].count/sum },
-    largest: { answer: results[results.length-1].answer, weight: results[results.length-1].count/sum },
+    smallest: { answer: results[0].answer, weight: 0.01 },
+    middle: { answer: results[1].answer, weight: 0.09 },
+    largest: { answer: results[results.length-1].answer, weight: 0.9 },
     method: 'wikipedia',
   }
   return final
