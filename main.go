2db18b2c43db3526a0c34efb5c032a57f9740d3c
Brady Killeen <brady.killeen@tcnbroadcasting.com>
2018-04-01 17:02:48 -0600

    added function to parse OCR output

--- main.go
+++ main.go
@@ -1,6 +1,12 @@
 package main
 
 import m "github.com/coltonmorris/ethics-review/methods"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 type QandA struct {
 	question string
@@ -8,46 +14,74 @@ type QandA struct {
 }
 
 type QuorumResults struct {
-  qna *QandA
-  methods []*m.MethodResults
-  finalAnswer []float64
+	qna         *QandA
+	methods     []*m.MethodResults
+	finalAnswer []float64
 }
-
-
 
 func runQuorum(qna *QandA) *QuorumResults {
 
-  quorum := &QuorumResults{
-    qna: qna,
-    methods: m.Methods,
-    // TODO calculate this by doing an average
-    finalAnswer: []float64{0.8, 0.15, 0.05}}
+	quorum := &QuorumResults{
+		qna:     qna,
+		methods: m.Methods,
+		// TODO calculate this by doing an average
+		finalAnswer: []float64{0.8, 0.15, 0.05}}
 
+	// TODO start here when you finish a method
+	// for _, method := range quorum.methods {
+	// go method.start(qna, doneChannel)
+	// }
+	// listen for a method to finish and print it's results
+	// go func() {
+	//  doneChannel <- result
+	//  PrintQuorumResults(calculateQuorumResults())
+	//
+	// }
 
-  // TODO start here when you finish a method   
-  // for _, method := range quorum.methods {
-    // go method.start(qna, doneChannel)
-  // }
-  // listen for a method to finish and print it's results
-  // go func() {
-  //  doneChannel <- result
-  //  PrintQuorumResults(calculateQuorumResults())
-  //  
-  // }
-
-
-
-  return quorum
+	return quorum
 }
 
+func parseQandA(path string) (QandA, error) {
+	var qna QandA
+
+	file, err := os.Open(path)
+	if err != nil {
+		return qna, err
+	}
+	defer file.Close()
+
+	questionParsed := false
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		text := scanner.Text()
+		text = strings.Trim(text, " \n\t")
+
+		if len(text) != 0 {
+			if !questionParsed {
+				qna.question += text
+			} else {
+				qna.answers = append(qna.answers, text)
+			}
+
+			if len(text) != 0 && text[len(text)-1] == '?' {
+				questionParsed = true
+			}
+		}
+	}
+
+	return qna, scanner.Err()
+}
 
 func main() {
-  // TODO parse ocr_output.txt to get these
-	qna := &QandA{
-		question: "Why are you who you are?",
-		answers:  []string{"because", "same", "raised by wolves"}}
+	qna, err := parseQandA("ocr_output.txt")
 
-  quorumResults := runQuorum(qna)
+	if err != nil {
+		fmt.Println("ERROR READING OCR OUTPUT:", err)
+		return
+	}
 
-  PrintQuorumResults(quorumResults)
+	quorumResults := runQuorum(&qna)
+
+	PrintQuorumResults(quorumResults)
 }
--- pretty-print.go
+++ pretty-print.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-  "fmt"
-  "github.com/fatih/color"
-  m "github.com/coltonmorris/ethics-review/methods"
+	"fmt"
+	m "github.com/coltonmorris/ethics-review/methods"
+	"github.com/fatih/color"
 )
 
 var yellow = color.New(color.FgYellow).SprintFunc()
@@ -12,34 +12,33 @@ var green = color.New(color.FgGreen).SprintFunc()
 var blue = color.New(color.FgBlue).SprintFunc()
 var black = color.New(color.FgBlack).Add(color.Underline).SprintFunc()
 
-
 func PrintQuorumResults(quorum *QuorumResults) {
-  PrintQandA(quorum.qna)
-  for _, method := range quorum.methods {
-    PrintMethodResults(quorum.qna, method)
-  }
-  PrintFinalAnswer(quorum.qna, quorum.finalAnswer)
+	PrintQandA(quorum.qna)
+	for _, method := range quorum.methods {
+		PrintMethodResults(quorum.qna, method)
+	}
+	PrintFinalAnswer(quorum.qna, quorum.finalAnswer)
 }
 
 func PrintFinalAnswer(qna *QandA, finalAnswer []float64) {
-  smallest, middle, largest := IndexOfSmallMiddleLarge(finalAnswer)
+	smallest, middle, largest := IndexOfSmallMiddleLarge(finalAnswer)
 
-  fmt.Println(black("Final Answer"))
-  fmt.Printf("\tSmallest: %s\n", red(finalAnswer[smallest], "\t ", qna.answers[smallest]))
-  fmt.Printf("\tMiddle:   %s\n", yellow(finalAnswer[middle], "\t ", qna.answers[middle]))
-  fmt.Printf("\tLargest:  %s\n", green(finalAnswer[largest], "\t ", qna.answers[largest]))
+	fmt.Println(black("Final Answer"))
+	fmt.Printf("\tSmallest: %s\n", red(finalAnswer[smallest], "\t ", qna.answers[smallest]))
+	fmt.Printf("\tMiddle:   %s\n", yellow(finalAnswer[middle], "\t ", qna.answers[middle]))
+	fmt.Printf("\tLargest:  %s\n", green(finalAnswer[largest], "\t ", qna.answers[largest]))
 }
 
 func PrintMethodResults(qna *QandA, method *m.MethodResults) {
-  smallest, middle, largest := IndexOfSmallMiddleLarge(method.Results)
+	smallest, middle, largest := IndexOfSmallMiddleLarge(method.Results)
 
-  fmt.Printf("Method: %s\n", blue(method.Name))
-  fmt.Printf("\tSmallest: %s\n", red(method.Results[smallest], "\t ", qna.answers[smallest]))
-  fmt.Printf("\tMiddle:   %s\n", yellow(method.Results[middle], "\t ", qna.answers[middle]))
-  fmt.Printf("\tLargest:  %s\n", green(method.Results[largest], "\t ", qna.answers[largest]))
+	fmt.Printf("Method: %s\n", blue(method.Name))
+	fmt.Printf("\tSmallest: %s\n", red(method.Results[smallest], "\t ", qna.answers[smallest]))
+	fmt.Printf("\tMiddle:   %s\n", yellow(method.Results[middle], "\t ", qna.answers[middle]))
+	fmt.Printf("\tLargest:  %s\n", green(method.Results[largest], "\t ", qna.answers[largest]))
 }
 
 func PrintQandA(qna *QandA) {
-  fmt.Printf("Question: %s\n", blue(qna.question))
-  fmt.Printf("Answers: \t%s\n \t\t%s\n \t\t%s\n", blue(qna.answers[0]), blue(qna.answers[1]), blue(qna.answers[2]))
+	fmt.Printf("Question: %s\n", blue(qna.question))
+	fmt.Printf("Answers: \t%s\n \t\t%s\n \t\t%s\n", blue(qna.answers[0]), blue(qna.answers[1]), blue(qna.answers[2]))
 }
